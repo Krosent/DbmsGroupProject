c7756d2f471d9dff0b1e4a244f5c2a7f76dfea96
Krosent <krosent2@gmail.com>
2020-12-24 16:01:12 +0400

    Starting implementing RandomReadingExperiment

---
+++ src/com/dbms/project/experiment2/RandomReadingExperiment.java
@@ -0,0 +1,15 @@
+package com.dbms.project.experiment2;
+
+import com.dbms.project.streamInterfaces.ReadStreamInterface;
+
+public class RandomReadingExperiment {
+    ReadStreamInterface version;
+    int sum = 0;
+    String pathImdb = ".\\src\\com\\dbms\\project\\data\\";
+
+    // f is the name of one of the csv files in the IMDB dataset, and j is a positive integer.
+    public void randjump(String f, int j) {
+        //int lenghtOfFile =;
+        //String line = version.readLn();
+    }
+}
